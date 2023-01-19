0abed3b687bfcfd7c1965af6a1b38e09076fc7af
Erik Fortune <ErikFortune@outlook.com>
2023-01-18 19:52:22 -0800

    ignore export files in coverage

--- src/utils/public.ts
+++ src/utils/public.ts
@@ -19,5 +19,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+// istanbul ignore file
 
 export { Normalizer, ValidationHelpers, ValidationHelpersConstructorParams } from './validationHelpers';
